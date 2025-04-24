3201b219c328148bd3406d69c7e208a75316ecea
Vaulry <marielaure-974@hotmail.fr>
2025-04-24 09:07:43 +0400

    page info

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ function App() {
   return (
     <>
       <h1>Répertoire Utilisateur</h1>
+      <ul>
+        <li>Nom</li>
+        <li>Prénom</li>
+        <li>Téléphone</li>
+      </ul>
+      <button>Mettre à jour</button>
+      <button>Favoris</button>
     </>
   )
 }
